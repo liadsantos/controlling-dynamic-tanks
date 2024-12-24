89f28d8a73d30c5d277294ebcfc59aa1631f9cc7
Lais Santos <laisisabelleas@gmail.com>
2024-12-24 18:02:18 -0300

    [WIP] control temperature and level tank 2

--- control_tanks.py
+++ control_tanks.py
@@ -8,7 +8,7 @@ import sys
 from scipy.integrate import odeint
 from multiprocessing.pool import ThreadPool
 
-def tank1_model(h, t, input, H, r, R, gamma):
+def tank1_model(h, t, input, out_tank2, H, r, R, gamma):
     """
     Define the right hand side of the tank dynamic equation.
     """
@@ -18,7 +18,7 @@ def tank1_model(h, t, input, H, r, R, gamma):
         sys.exit()
 
     A = np.pi * (r + (R-r)/H * h)**2
-    dhdt = (input - gamma * np.sqrt(h)) / A     # @TODO: don't forget to add the -q_(i+1)
+    dhdt = (input - gamma * np.sqrt(h) - out_tank2) / A
 
     return dhdt
 
@@ -48,8 +48,8 @@ def thread_tanks(h0, Q_i, Q_hot):
     """
     # --- Define the parameters
     # Discharge coefficients
-    gamma_1 = 5.0
-    gamma_2 = 0.5
+    gamma_1 = 5
+    gamma_2 = 20
     gamma_3 = ...   # @TODO: to define
  
     # Radios (all tanks have the same size)
@@ -60,9 +60,9 @@ def thread_tanks(h0, Q_i, Q_hot):
     H = 70
 
     # Simulation time
-    simulation_time = np.linspace(0.0, 5.0, 50)    
+    simulation_time = np.linspace(0.0, 5.0, 100)    
 
-    h1 = odeint(tank1_model, h0[0], simulation_time, args=(Q_i[0], H, r, R, gamma_1))
+    h1 = odeint(tank1_model, h0[0], simulation_time, args=(Q_i[0], Q_i[1], H, r, R, gamma_1))
     h2 = odeint(tank2_model, h0[1], simulation_time, args=(Q_i[1], Q_hot, H, r, R, gamma_2))
     h3 = 0
 
@@ -93,45 +93,71 @@ def thread_control():
     level_tank1 = list()
     time_steps = list()
     valve_1 = list()
+    valve_2 = list()
+    valve_hot = list()
+    temp_tank2 = list()
+    level_tank2 = list()
     stop_valve1 = False
     eps = 0.1
+    delta = 1e-4
 
-    for i in range(50):
+    for i in range(100):
         # Control law tank 1
         T[0] = 25
-        erro1 = h_ref - h0[0]
-        if erro1 < eps:
-            stop_valve1 = True      # if the level is ok for tank 1, turn off input valve
-            Q_i[0] = 0
+        error1 = h_ref - h0[0]
+        # if error1 < eps:
+        #     stop_valve1 = True      # if the level is ok for tank 1, turn off input valve
+        #     Q_i[0] = 0
     
-        elif erro1 >= eps and not stop_valve1:
-            controler1 = 20
-            Q_i[0] = erro1 * controler1
+        # elif error1 >= eps and not stop_valve1:
+        #     controller1 = 20
+        #     Q_i[0] = error1 * controller1
+        controller1 = 20
+        Q_i[0] = error1 * controller1
 
         # Control law tank 2
         # How the temperature is related to valve hot?
+        error2_level = h_ref - h0[1]
+        T[1] = (Q_i[1] * T[0] + Q_hot * T_hot) / (Q_i[1] + Q_hot + delta)
+        error2_temp = T_ref - T[1]
+        controller2_level = 20
+        controller2_temp = 10
+        Q_i[1] = error2_level * controller2_level
+        Q_hot = error2_temp * controller2_temp      # @TODO: another controller - not sufficient to control temperature
 
         time_steps.append(i)
         level_tank1.append(h0[0])
         valve_1.append(Q_i[0])
+        valve_2.append(Q_i[1])
+        valve_hot.append(Q_hot)
+        temp_tank2.append(T[1])
+        level_tank2.append(h0[1])
 
         logging.info('Calling process thread.')
         # tanks = threading.Thread(target=thread_tanks, args=(h0, Q_i))
         # tanks.start()
         # tanks.join()
         pool = ThreadPool(processes=1)
-        new_h0 = pool.apply_async(thread_tanks, args=(h0, Q_i))
+        new_h0 = pool.apply_async(thread_tanks, args=(h0, Q_i, Q_hot))
         h1, h2, h3, sim_time = new_h0.get()
         h0[0] = h1[-1]
+        h0[1] = h2[-1]
 
         plt.clf()
 
-        plt.subplot(2,1,1)
+        plt.subplot(3,1,1)
         plt.plot(time_steps, level_tank1, label='Level tank 1')
+        plt.plot(time_steps, level_tank2, label='Level tank 2')
         plt.legend()
 
-        plt.subplot(2,1,2)
+        plt.subplot(3,1,2)
         plt.plot(time_steps, valve_1, label='Valve 1')
+        plt.plot(time_steps, valve_2, label='Valve 2')
+        plt.plot(time_steps, valve_hot, label='Valve hot')
+        plt.legend()
+
+        plt.subplot(3,1,3)
+        plt.plot(time_steps, temp_tank2, label='Temperature tank2')
         plt.legend()
 
         plt.pause(1.0)
